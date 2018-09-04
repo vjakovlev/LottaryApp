295e92859659c7ce900a8e9e277a898b3016756a
DZlatanovski <haxors_free@hotmail.com>
2018-09-04 19:23:07 +0200

    new commit

---
+++ Lottary/Lottary.Services/LottaryService.cs
@@ -0,0 +1,71 @@
+﻿using Lottary.Data;
+using Lottary.DataModel;
+using Lottary.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lottary.Services
+{
+    public class LottaryService : IlottaryService
+    {
+        public AwardModel CheckCode(UserCodeModel userCodeModel)
+        {
+            var _codeRepository = new Repository<Code>(new DbContext("LottaryDb"));
+
+            var code = _codeRepository.GetAll().FirstOrDefault(x => x.CodeValue == userCodeModel.Code.CodeValue);
+
+            if (code == null)
+            {
+                throw new ApplicationException("Invalid Code.");
+            }
+
+            if (code.IsUsed)
+            {
+                throw new ApplicationException("Code is used.");
+            }
+
+            var userCode = new UserCode
+            {
+                Code = code,
+                Email = userCodeModel.Email,
+                FirstName = userCodeModel.Email,
+                LastName = userCodeModel.LastName,
+                SentAt = DateTime.Now
+            };
+
+            var _userCodeRepository = new Repository<UserCode>(
+                new DbContext("LottaryDb"));
+            _userCodeRepository.Insert(userCode);
+
+            Award award = new Award()
+            {
+                RuffleType = (byte)RuffledType.Immediate,
+                AwardName = "Another 0.5 bottle",
+                Quantity = 1
+            };
+
+            if (code.IsWinning)
+            {
+                var userCodeAward = new UserCodeAward
+                {
+                    Award = award,
+                    UserCode = userCode,
+                    WonAt = DateTime.Now
+                };
+
+                var _userCodeAwardRepository = new Repository<UserCodeAward>(new DbContext("LottaryDb"));
+                _userCodeAwardRepository.Insert(userCodeAward);
+            }
+
+            return new AwardModel()
+            {
+                AwardName = award.AwardName,
+                AwardDescription = award.AwardDescription
+            };
+        }
+    }
+}
---
+++ Lottary/Lottary.ViewModel/AwardModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lottary.ViewModel
+{
+    public class AwardModel
+    {
+        public string AwardName { get; set; }
+
+        public string AwardDescription { get; set; }
+    }
+}
